ea0f55296bc09266a93e0801d86b579312c4a7cc
Metelity <kirillharin3144@yandex.ru>
2025-03-22 00:23:13 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blume
+
+Blume – is a simple and convenient library for working with databases. It allows you to create databases, tables and control it directly in the code. Blume is ideal for small projects where complex functionality is not required
+
+Metelity Lib Company @2025
+
+## Install
+
+Установите библиотеку с помощью pip:
+
+```bash
+git clone https://github.com/metelity/blume
+cd blume
+python install.py
+```
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blume",
+    version="1.0.0",
+    author="metelity",
+    description="A database management library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/metelity/blume",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
