9906a1ddc2080e4537c9841ea2a8ed653972d6d5
Metelity <kirillharin3144@yandex.ru>
2025-03-22 00:49:23 +0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,3 @@
+import os; os.system("cls|clear"); os.system("python3 -m pip uninstall blume"); os.system("cls|clear"); os.system("python3 -m pip cache purge"); os.system("cls|clear"); os.system("python3 -m pip install ."); os.system("cls|clear"); print("[!] Blume installed!")
+
+# Metelity Lib Company ©2025
